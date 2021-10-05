f5930888efd091373c123fa1e90142020d7c59d3
Nicolas Trierveiler <nicolastrierveiler@gmail.com>
2021-10-04 22:40:03 -0300

    WIP | First Commit | Setup SASS, NUXT, VUE, Base

---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+    <body>
+  <section class="header">
+    <img class="header__banner" src="/assets/images/fixed-banner.jpg" alt="Só para não profissionais da saúde residentes do Brasil">
+    <div class="header__nav"> 
+      <img src="\img\dulco-logo.png" alt="Logo Dulco">
+      <ul class="header__list">
+        <li class="list__item">Dulcolax®</li>
+        <li class="list__item">Guttalax®</li>
+        <li class="list__item">Dulcogas®</li>
+        <li class="list__item">Dicas de Saúde</li>
+        <li class="list__item">Perguntas Frequentes</li>
+        <li class="list__item">Fale Conosco</li>
+        <li class="list__item">Onde Comprar</li>
+      </ul>
+    </div>
+  </section>
+  <section class="banner">
+    <div class="banner__title">
+      <h1 class="banner__text">Dulcolax®</h1>
+      <h2 class="banner__subtext">Comprimidos revestidos</h2>
+    </div>
+    <p class="banner__promo">
+      Alívio eficaz e previsível da prisão de ventre, da noite para o dia¹,².
+    </p>
+    <img class="banner__product" src="/img/dulcolax-comprimido.pNg" alt="Dulcolax Bisacodil Comprimido">
+    <button class="banner__buy btn btn--green"> 
+      <i class="fas fa-shopping-cart"></i>
+      COMPRAR 
+    </button>
+    <div class="banner__link">
+      <i class="fas fa-bars"></i>
+      Leia a bula
+    </div>
+    <p class="banner__text">
+      Alívio eficaz e previsível da prisão de ventre, da noite para o dia¹,².
+    </p>
+  </section>
+  <section class="tabs">
+    <button class="tabs__accordion">Saiba Mais</button>
+<div class="tabs__panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="tabs__accordion">Perguntas Frequentes</button>
+<div class="tabs__panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="tabs__accordion">Vídeo</button>
+<div class="tabs__panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="tabs__accordion">Nossos Laxantes</button>
+<div class="tabs__panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+  </section>
+  
+  <h2>Conheça também</h2>
+  <section class="CTA">
+    <img src="/assets/images/dulcogotas-default.png" alt="Guttalax Gotas">
+    <button class="CTA__buy-button">
+      <i class="fas fa-shopping-cart"></i>
+      <p class="CTA__text">Comprar</p> 
+    </button>
+    <button class="CTA__more-button">
+      <p class="CTA__text">Saiba Mais</p> 
+    </button>
+  </section>
+
+  <section class="footer">
+    <img src="" alt="Logo Sanofi" class="footer__logo">
+    <ul class="footer__nav">
+      <li class="footer__item"> Mapa do Site </li>
+      <li class="footer__item"> Política de Privacidade</li>
+      <li class="footer__item"> Termos e Condições </li>
+      <li class="footer__item"> Política de Cookies </li>
+      <li class="footer__item"> Fale Conosco </li>
+    </ul>
+    <p class="footer__text">
+      Só para residentes no Brasil.COPYRIGHT® 2018 SANOFI. 
+      TODOS OS DIREITOS RESERVADOS.Dulcolax® - UM PRODUTO SANOFI. 
+      (Abril/2018)Av. das Nações Unidas, 14401 - 
+      Chácara Santo Antônio, São Paulo - SP, 04794-000.CNPJ: 02.685.377/0001-57Última atualização (13/04/1987).
+    </p>
+    <br>
+    <p class="footer__text">
+      DULCOGAS® (simeticona). Indicação: alívio dos sintomas relacionados ao excesso de gases no aparelho digestivo, 
+      que geram flatulência, desconforto abdominal, aumento de volume abdominal, dor ou cólicas no abdômen. 
+      Preparo do paciente a ser submetido a endoscopia digestiva e/ou colonoscopia.
+      MEDICAMENTO DE NOTIFICAÇÃO SIMPLIFICADA RDC Nº 199 de 2006. AFE nº: 1.01300.3.
+      O USO DO MEDICAMENTO PODE TRAZER ALGUNS RISCOS. Leia atentamente a bula. SE PERSISTIREM OS SINTOMAS, O MÉDICO DEVERÁ SER CONSULTADO.
+    </p>
+    <p class="footer__text">
+      DULCOLAX® (bisacodil). Indicação: casos de constipação intestinal, no preparo para procedimentos diagnósticos,
+      no pré e pós-operatório e em condições que exigem uma evacuação facilitada.
+      MS 1.0367.0022. O USO DO MEDICAMENTO PODE TRAZER ALGUNS RISCOS. Leia atentamente a bula.
+      SE PERSISTIREM OS SINTOMAS, O MÉDICO DEVERÁ SER CONSULTADO.
+    </p>
+    <p class="footer__text">
+      GUTTALAX® (picossulfato de sódio). Indicação: para o tratamento da constipação e para facilitar a evacuação intestinal.
+      MS 1.1300.1167. O USO DO MEDICAMENTO PODE TRAZER ALGUNS RISCOS. Leia atentamente a bula.
+      SE PERSISTIREM OS SINTOMAS, O MÉDICO DEVERÁ SER CONSULTADO.
+    </p>
+    <p class="footer__text">
+      Referências bibliográficas: 1- Kienzle HS, Vix JM, Schuijt C et al. Efficacy and safety of bisacodyl in the acute treatment of constipation: a double-blind, randomized, placebo-controlled study. Aliment Pharmacol Ther 2006; 23 (10):1479-1488; 2- Dulcolax® (Bula do produto].Sanofi-Aventis Farmacêutica Ltda. 3- IMS MIDAS QTRLY Sales - Junho 2018 - Bisacodyl Global Market. 4- Wulkow R, Vix JM, Schuijt C et al. Randomised, placebo-controlled, double-blind study to investigate the efficacy and safety of the acute use of sodium picosulphate in patients with chronic constipation. Int J Clin Pract 2007; 61 (6):944-950. 5- Guttalax® (Bula do produto]. Sanofi-Aventis Farmacêutica Ltda. 6- Lacy BE Mearin F, Chang L et al. Bowel Disorders.Gastroenterology 2016;150:1393–1407 7- Bharucha AE et al..GASTROENTEROLOGY 2013;144:211–217 8- DulcoGas® (Bula do produto). Sanofi-Aventis Farmacêutica Ltda.
+    </p>
+
+  </section>
+</body>
+</template>
+<style>
+   @import 'assets/css/main.css';
+ </style>
